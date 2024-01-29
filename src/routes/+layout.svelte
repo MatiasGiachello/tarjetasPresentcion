5732357e3e824aff4d61c10af5a79236b572dec5
Matias Giachello <matiasgiachello04@gmail.com>
2024-01-29 08:53:17 -0300

    presentacion

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<header>
+	<h1>SURFACTAN SA</h1>
+</header>
+
+<slot />
+
+<style>
+	header {
+		background-color: #182491;
+		color: #fff;
+		padding: 10px;
+		width: 100%;
+		box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+		text-align: center;
+	}
+</style>
